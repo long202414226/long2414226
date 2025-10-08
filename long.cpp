1dce8efb591ed1992287d080202be9ee14c0883e
long202414226 <uchihalong511@gmail.com>
2025-10-08 23:54:00 +0700

    Update long.cpp

--- long.cpp
+++ long.cpp
@@ -4,20 +4,5 @@ using namespace std;
 int main() {
     int n;
     cout << "Nhap n: ";
-    cin >> n;
-
-    int a = 0, b = 1, fib;
-
-    if (n == 0) fib = a;
-    else if (n == 1) fib = b;
-    else {
-        for (int i = 2; i <= n; i++) {
-            fib = a + b;
-            a = b;
-            b = fib;
-        }
-    }
-
-    cout << "So Fibonacci thu " << n << " la: " << fib << endl;
     return 0;
 }
